f3cae793c16609a27f93f3d6f2c61370f51de16a
Mark Bekker <mb6386@nyu.edu>
2019-10-24 02:18:59 -0400

    changes

--- Efficient_Eater/main/views.py
+++ Efficient_Eater/main/views.py
@@ -52,12 +52,10 @@ def calculator(request, restaurant_slug=""):
     searched_items = []
     for i in range(1,21):
         searched_items.append(request.GET.get(f'itemName{i}'))
-
+    print(f"\n\n\n {searched_items} \n\n\n")
     searched_items_quantity = []
     for i in range(1,21):
         searched_items_quantity.append(request.GET.get(f'itemQuantity{i}'))
-
-    output = calculator_order(searched_items, searched_items_quantity, len(searched_items))
 
     chosen_restaurant = "All"
     if restaurant_slug != "":
@@ -69,11 +67,14 @@ def calculator(request, restaurant_slug=""):
     options = []
     for i in range(1,21):
         options.append(i)
+    order = calculator_order(searched_items, searched_items_quantity, len(searched_items))
+    output = calculate_nutrition(order)
     context={"restaurants": restaurants,
              "items": items,
              "chosen_restaurant": chosen_restaurant,
              "item_list": item_list,
              "options": options,
+             "order": order,
              "output": output}
     return render(request, template_name, context)
 
@@ -82,11 +83,28 @@ def calculator_order(searched_items, searched_items_quantity, length):
     valid_counter = 1
     for i in range(length):
         if is_valid_queryparam(searched_items[i]):
-            if len((Item.objects.filter(name__iexact=searched_items[i]))) > 0:
-                order.append(f"<b>{valid_counter}</b>. {Item.objects.filter(name__iexact=searched_items[i])[0]} ({searched_items_quantity[i]})")
+            if len((Item.objects.filter(id=searched_items[i]))) == 1:
+                order.append([valid_counter,f"{Item.objects.filter(id=searched_items[i])[0]}",searched_items_quantity[i]])
                 valid_counter+=1
-
     return order
+
+def calculate_nutrition(order):
+    calories = 0
+    total_fat = 0
+    sat_fat = 0
+    trans_fat = 0
+    cholesterol = 0
+    sodium = 0
+    carbs = 0
+    fiber = 0
+    sugar = 0
+    protein = 0
+    floz = 0
+    #for itemList in order:
+        #print(Item.objects.filter(id=itemList[1])[0].calories)
+        #calories += (itemList[2] * Item.objects.filter(name=itemList[1])[0].calories)
+
+    print(f"\n {calories} \n")
 
 def methodology(request):
     return render(request = request,
